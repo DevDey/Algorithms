a87c45acff646a38f670836a93163ecb53bc0136
DevDey <dey.77@buckeyemail.osu.edu>
2020-07-28 17:58:14 -0400

    Quicksort using last element as pivot

--- Project_1/QuickSort.py
+++ Project_1/QuickSort.py
@@ -1,18 +1,30 @@
-# Takes two sorted arrays and merges them together
+"""Quicksort"""
 
 
-def partition(arr, last):
-    for i in range(last):
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i = i + 1
+            swap = arr[i]
+            arr[i] = arr[j]
+            arr[j] = swap
+    swap = arr[i + 1]
+    arr[i + 1] = arr[high]
+    arr[high] = swap
+    return i + 1
 
-        # Reursively merge sorts an array
 
+def quickSort(arr, low, high):
 
-def quickSort(arr):
-    if len(arr) > 1:
-        last = len(arr)
-        pivot = partition(arr, last)
-        quickSort(arr[pivot:])
-        quickSort(arr[:pivot])
+    if (low < high):
+        """ pi is partitioning index, arr[pi] is now
+           at right place """
+        pi = partition(arr, low, high)
+        quickSort(arr, low, pi - 1)  # Before pi
+        quickSort(arr, pi + 1, high)  # After pi
+
 
 # Print array
 
@@ -27,6 +39,6 @@ arr = [5, 2, 3, 4, 1, 1, 2, 4, 50, 23, 12, 52, 12, 422, 112, 893212, 21,
        23, 4, 4214, 421, 21312, 241, -1, -2311, 0, -213, 41212, 21, 2145, 657]
 print("Before Sorting: ")
 printArray(arr)
-quickSort(arr)
+quickSort(arr, 0, len(arr)-1)
 print("After Sorting: ")
 printArray(arr)
